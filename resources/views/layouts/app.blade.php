c3ed47f5dfdbf9d88c97a12e7ada054bb8b0389c
kalai2903 <skalaiselviskalaiselvi1@gmail.com>
2026-05-03 09:33:08 +0530

    first commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,700;0,900;1,400&family=DM+Sans:wght@300;400;500&family=DM+Mono:wght@400;500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/portfolio.css') }}">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+<script src="{{ asset('js/app.js') }}"></script>
 
 </head>
 <body>
